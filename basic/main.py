ae66d2daabfe1588d06bc937043ee0e185bade75
LOGESH <logsak.12.34@gmail.com>
2022-07-17 08:59:18 +0530

    added worker funcs in a class

--- basic/main.py
+++ basic/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from data import ToDo_List, Priority, Tag, TODO
-from workers import get_todo_item, add_todo_item, update_todo_item, patch_todo_item, delete_todo_item, get_todo_by_id
+from workers import TodoWorkers
 
 todo = FastAPI()
 
@@ -17,39 +17,39 @@ async def list_todos():
 
 @todo.get('/todo/priority/{todo_priority}')
 async def list_todo_for_given_priority(todo_priority: Priority):
-    return {"todo_list": get_todo_item(priority=todo_priority)}
+    return {"todo_list": TodoWorkers.get_todo_item(priority=todo_priority)}
 
 
 @todo.get('/todo/tag/{todo_tag}')
 async def list_todo_for_given_priority(todo_tag: Tag):
-    return {"todo_list": get_todo_item(tag=todo_tag)}
+    return {"todo_list": TodoWorkers.get_todo_item(tag=todo_tag)}
 
 
 @todo.get('/todo/filter')
 async def filter_todo(priority: Priority, tag: Tag):
-    return {"todo_list": get_todo_item(priority=priority, tag=tag)}
+    return {"todo_list": TodoWorkers.get_todo_item(priority=priority, tag=tag)}
 
 
 @todo.post("/todo/create")
 async def create_todo(payload: TODO):
-    return {"id": add_todo_item(payload), "status": "Success"}
+    return {"id": TodoWorkers.add_todo_item(payload), "status": "Success"}
 
 
 @todo.get('/todo/{todo_id}')
 async def get_todo(todo_id: int):
-    return get_todo_by_id(todo_id)
+    return TodoWorkers.get_todo_by_id(todo_id)
 
 
 @todo.put('/todo/{todo_id}')
 async def update_todo(todo_id: int, payload: TODO):
-    return update_todo_item(payload, todo_id)
+    return TodoWorkers.update_todo_item(payload, todo_id)
 
 
 @todo.patch('/todo/{todo_id}')
 async def patch_todo(todo_id: int, todo_name: str = None, priority: Priority = None, tag: Tag = None):
-    return patch_todo_item(todo_id=todo_id, todo_name=todo_name, priority=priority, tag=tag)
+    return TodoWorkers.patch_todo_item(todo_id=todo_id, todo_name=todo_name, priority=priority, tag=tag)
 
 
 @todo.delete('/todo/{todo_id}')
 async def delete_todo(todo_id: int):
-    return delete_todo_item(todo_id)
+    return TodoWorkers.delete_todo_item(todo_id)
